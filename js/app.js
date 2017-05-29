4743b7a48ecb677f1d1a74eb8d123caf29446a54
iofly <iofly@noreply.users.github.com>
2017-05-29 19:33:57 +0100

    Added some text functions

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ rapidApp.controller("MainController", function ($scope, $http, $uibModal, $sce) 
        formvars.EditorIndexTarget = $editorIndexTarget;
        formvars.StringsRequired = [{Label: "Text to find", Value: ""}, {Label: "Replace with", Value: ""}];
        $scope.showGetVarsModal(formvars);
-    }
+    };
 
 	$scope.showGetVarsModal = function (formvars) {
 
@@ -75,13 +75,145 @@ rapidApp.controller("MainController", function ($scope, $http, $uibModal, $sce) 
          });
 	};
 
+    $scope.trimStrings = function ($editorIndex1, $editorIndex2) {
 
+        var txt1=null;
+        if($editorIndex1 == 1) txt1 = $scope.editorText1;
+        else txt1 = $scope.editorText2;
+
+        txt1 = $scope.TrimStrings_String(txt1);
+
+        if($editorIndex2 == 1) $scope.editorText1 = txt1;
+        else $scope.editorText2 = txt1;
+    };
+
+    $scope.sortLines = function ($sortOrder, $editorIndex1, $editorIndex2) {
+        $sortOrder = $sortOrder || 1;
+        var txt1=null;
+        if($editorIndex1 == 1) txt1 = $scope.editorText1;
+        else txt1 = $scope.editorText2;
+
+        txt1 = $scope.SortStrings_String(txt1, $sortOrder);
+
+        if($editorIndex2 == 1) $scope.editorText1 = txt1;
+        else $scope.editorText2 = txt1;
+    };
+
+    $scope.uniquizeLines = function ($editorIndex1, $editorIndex2) {
+        var txt1=null;
+        if($editorIndex1 == 1) txt1 = $scope.editorText1;
+        else txt1 = $scope.editorText2;
+
+        txt1 = $scope.UniquizeStrings_String(txt1);
+
+        if($editorIndex2 == 1) $scope.editorText1 = txt1;
+        else $scope.editorText2 = txt1;
+
+    };
+
+    $scope.extractLines = function ($editorIndex1, $editorIndex2) {
+
+    };
+
+    $scope.stripHTML = function ($editorIndex1, $editorIndex2) {
+        var txt1=null;
+        if($editorIndex1 == 1) txt1 = $scope.editorText1;
+        else txt1 = $scope.editorText2;
+
+        txt1 = $scope.StripHTML(txt1);
+        console.log("stripHTML : " + txt1);
+        if($editorIndex2 == 1) $scope.editorText1 = txt1;
+        else $scope.editorText2 = txt1;
+    };
+
+    $scope.htmlEncode = function ($editorIndex1, $editorIndex2) {
+        var txt1=null;
+        if($editorIndex1 == 1) txt1 = $scope.editorText1;
+        else txt1 = $scope.editorText2;
+
+        var elm = document.createElement("div");
+        elm.innerText = elm.textContent = txt1;
+        txt1 = elm.innerHTML;
+        delete elm;
+        if($editorIndex2 == 1) $scope.editorText1 = txt1;
+        else $scope.editorText2 = txt1;
+    };
+
+    $scope.htmlDecode = function ($editorIndex1, $editorIndex2) {
+
+    };
+
+    $scope.urlEncode = function ($editorIndex1, $editorIndex2) {
+
+    };
+
+    $scope.urlDecode = function ($editorIndex1, $editorIndex2) {
+
+    };
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
         $scope.TrimStringsRight_String = function ($s, $substring) {
             $s = $s || "";
             var strings = $s.toStrings();
             strings = $scope.TrimStringsRight_Array(strings, $substring);
+            var res = strings.join("\n");
+            return res;
+        };
+
+        $scope.TrimStrings_String = function ($s) {
+            $s = $s || "";
+            var strings = $s.toStrings();
+            strings = $scope.TrimStrings_Array(strings);
+            var res = strings.join("\n");
+            return res;
+        };
+
+        $scope.SortStrings_String = function ($s, $sortDirection) {
+            $s = $s || "";
+            $sortDirection = $sortDirection || 1;
+            var strings = $s.toStrings();
+            
+            strings.sort();
+            
+            if($sortDirection==2)
+                strings.reverse();
+            
             var res = strings.join("\n");
             return res;
         };
@@ -97,7 +229,39 @@ rapidApp.controller("MainController", function ($scope, $http, $uibModal, $sce) 
             }
             
             return $strings;
-        }
+        };
+
+        $scope.TrimStrings_Array = function ($strings) {
+            $strings = $strings || [];
+            var n = 0;
+            for( n = 0 ; n < $strings.length ; n++ ){
+                $strings[n] = $strings[n].trim();
+            }
+            
+            return $strings;
+        };
+
+        $scope.UniquizeStrings_String = function ($s) {
+            $s = $s || "";
+            var strings = $s.toStrings();
+            var n = 0;
+            for( n = strings.length-1 ; n >=0  ; n-- ){
+                if (strings.indexOf(strings[n])!=n)
+                {
+                    strings.splice(n, 1);
+                }
+            }
+            var res = strings.join("\n");
+            return res;
+        };
+
+        $scope.StripHTML = function($html){
+            var div = document.createElement("div");
+            div.innerHTML = $html;
+            return div.textContent || div.innerText || "";
+        };
+
+        
 
 
 });
